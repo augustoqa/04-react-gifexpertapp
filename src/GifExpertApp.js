3c6120c3c95b22a220aeca5a2ab3ba6316a07d22
Cesar Acual <chechaacual@gmail.com>
2020-09-01 15:40:39 -0600

    Comunicacion entre componentes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
 
-export const GifExpertApp = () => {
+export const GifExpertApp = ( value ) => {
 
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
     return (
         <>
             <h2>GifExpertApp</h2>
-            <AddCategory />
+            <AddCategory setCategories={ setCategories } />
             <hr />
 
             <ol>
