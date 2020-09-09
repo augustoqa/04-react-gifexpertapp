b8af829568e47c9aaa36dd58940b106ce9a6c522
Cesar Acual <augustoacual@gmail.com>
2020-09-09 12:02:58 -0600

    docs agregado

---
+++ docs/precache-manifest.1cb1a8b8c7f81222186676230d4febf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2331cc1ba4559ceef2b83440506d7fd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b3953ff3a7208c94db6",
+    "url": "/static/css/main.a4ca3ba7.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b3953ff3a7208c94db6",
+    "url": "/static/js/main.d8aacbfa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
