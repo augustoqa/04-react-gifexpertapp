3fe0c73ef0bc0f9e9a4e4b9c03827ed27bf1381e
Cesar Acual <augustoacual@gmail.com>
2020-10-15 12:20:01 -0600

    Pruebas en el helper getGifs

---
+++ src/tests/helpers/getGifs.test.js
@@ -0,0 +1,20 @@
+import { getGifs } from '../../helpers/getGifs';
+
+describe('Pruebas con getGifs Fetch', () => {
+
+    test('debe de traer 10 elementos', async () => {
+
+        const gifs = await getGifs('One Punch');
+
+        expect( gifs.length ).toBe( 10 );
+
+    })
+
+    test('debe de traer 0 elementos, si no recibe una categoria', async () => {
+
+        const gifs = await getGifs('');
+        expect( gifs.length ).toBe( 0 );
+
+    })
+
+})
